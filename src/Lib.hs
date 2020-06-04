430e496a111b54928d80bb9eca90ab8e341046e9
Guillermo Felipetti <guill388@hotmail.com>
2020-06-04 17:34:01 -0300

    3

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,8 +11,8 @@ data Jugador = UnJugador {
 } deriving (Eq, Show)
 
 data Habilidad = Habilidad {
-  fuerzaJugador :: Int,
-  precisionJugador :: Int
+  fuerzaJugador :: Float,
+  precisionJugador :: Float
 } deriving (Eq, Show)
 
 -- Jugadores de ejemplo
@@ -21,9 +21,9 @@ todd = UnJugador "Todd" "Ned" (Habilidad 15 80)
 rafa = UnJugador "Rafa" "Gorgory" (Habilidad 10 1)
 
 data Tiro = UnTiro {
-  velocidad :: Int,
-  precision :: Int,
-  altura :: Int
+  velocidad :: Float,
+  precision :: Float,
+  altura :: Float
 } deriving (Eq, Show)
 
 type Puntos = Int
@@ -36,4 +36,35 @@ mayorSegun f a b
   | f a > f b = a
   | otherwise = b
 -}
+
+--PUNTO 1--
+
+type Palo = (Habilidad -> Tiro)
+
+putter :: Palo
+putter habilidad = UnTiro 10 (modificarPrecisionJugador 2 habilidad)  0
+
+madera :: Palo
+madera habilidad = UnTiro 100 (modificarPrecisionJugador 0.5 habilidad) 0
+
+hierros :: Float -> Palo
+hierros n habilidad = UnTiro (modificarFuerzaJugador n habilidad) (modificarPrecisionJugador (1 / n) habilidad) (n - 3)
+
+modificarPrecisionJugador :: Float -> Habilidad -> Float
+modificarPrecisionJugador n  = (*n).precisionJugador 
+
+modificarFuerzaJugador :: Float -> Habilidad -> Float
+modificarFuerzaJugador n = (*n).fuerzaJugador
+
+type Palos = [Palo]
+
+
+--PUNTO 2--
+
+golpe :: Jugador -> Palo -> Tiro
+golpe jugador palo = palo (habilidad jugador)
+
+--PUNTO 3--
+
+
 
