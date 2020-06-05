2f8e615e3d40519c183ac5c44a1b754ea94b336d
Guillermo Felipetti <guill388@hotmail.com>
2020-06-04 21:26:07 -0300

    4

--- src/Lib.hs
+++ src/Lib.hs
@@ -84,7 +84,7 @@ type Obstaculo = (Tiro -> Tiro)
 
 tunelConRampita :: Obstaculo
 tunelConRampita  tiro 
- | (condicionParaSuperarloEnPrecision (>90) tiro) && (condicionParaSuperarloEnAltura (==0) tiro) = modificarVelocidadyPrecision 2 tiro
+ | (condicionParaSuperarloEnPrecision (>90) tiro) && (condicionParaSuperarloEnAltura (==0) tiro) = efectoTunelConRampita tiro
  | otherwise = tiroDetenido
 
 condicionParaSuperarloEnVelocidad :: (Float -> Bool) -> Tiro -> Bool
@@ -96,30 +96,23 @@ condicionParaSuperarloEnPrecision condicion = condicion.precision
 condicionParaSuperarloEnAltura :: (Float -> Bool) -> Tiro -> Bool
 condicionParaSuperarloEnAltura condicion = condicion.altura
 
-modificarVelocidadyPrecision :: Float -> Tiro -> Tiro
-modificarVelocidadyPrecision n tiro = UnTiro {velocidad = (velocidad tiro) * n , precision = 100, altura = 0}
+efectoTunelConRampita :: Tiro -> Tiro
+efectoTunelConRampita  tiro = UnTiro {velocidad = (velocidad tiro) * 2 , precision = 100, altura = 0}
 
 tiroDetenido = UnTiro 0 0 0 
-{-
-Lo que nos interesa de los distintos obstáculos es si un tiro puede superarlo, 
-y en el caso de poder superarlo, cómo se ve afectado dicho tiro por el obstáculo. 
-En principio necesitamos representar los siguientes obstáculos:
-Un túnel con rampita sólo es superado si la precisión es mayor a 90 yendo al ras del suelo, 
-independientemente de la velocidad del tiro. Al salir del túnel la velocidad del tiro se duplica, 
-la precisión pasa a ser 100 y la altura 0.
-Una laguna es superada si la velocidad del tiro es mayor a 80 y tiene una altura de entre 1 y 5 metros. 
-Luego de superar una laguna el tiro llega con la misma velocidad y precisión, 
-pero una altura equivalente a la altura original dividida por el largo de la laguna.
-Un hoyo se supera si la velocidad del tiro está entre 5 y 20 m/s yendo al ras del suelo con una precisión mayor a 95. 
-Al superar el hoyo, el tiro se detiene, quedando con todos sus componentes en 0.
--}
---laguna :: Obstaculo
 
 laguna :: Float -> Tiro -> Tiro
 laguna largoLaguna tiro
-  | (condicionParaSuperarloEnVelocidad (>80) tiro) && (condicionParaSuperarloEnAltura (<5) tiro) =  (efectoLaguna largoLaguna) tiro
+  | (condicionParaSuperarloEnVelocidad (>80) tiro) && condicionParaSuperarloEnAltura (between 1 5) tiro =  (efectoLaguna largoLaguna) tiro
   | otherwise = tiroDetenido
 
 efectoLaguna :: Float -> Tiro -> Tiro
 efectoLaguna largoLaguna tiro = UnTiro {velocidad = velocidad tiro, precision = precision tiro, altura = ((/largoLaguna).altura) tiro}
+
+hoyo :: Obstaculo
+hoyo tiro
+ | (condicionParaSuperarloEnVelocidad (between 5 20) tiro) && (condicionParaSuperarloEnPrecision (>95) tiro) && (condicionParaSuperarloEnAltura (==0) tiro) = tiroDetenido
+ | otherwise = tiro
+ 
+--PUNTO 4--
 
